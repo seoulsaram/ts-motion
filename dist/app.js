3c85892cdfc1f740f6a37e70ac76de4b1e3a107f
seoulsaram <kaste2233@gmail.com>
2021-08-21 18:20:35 +0900

    리팩토링

--- dist/app.js
+++ dist/app.js
@@ -1,32 +1,37 @@
+import { TextSectionInput } from './components/dialog/input/text-input.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { PageComponent, PageItemComponent } from './components/page/page.js';
 import { InputDialog } from './components/dialog/dialog.js';
+import { MediaSectionInput } from './components/dialog/input/media-input.js';
 class App {
-    constructor(appRoot) {
+    constructor(appRoot, dialogRoot) {
+        this.dialogRoot = dialogRoot;
         this.page = new PageComponent(PageItemComponent);
         this.page.attachTo(appRoot);
-        const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        this.page.addChild(image);
-        const video = new VideoComponent('Video Title', 'https://youtu.be/K3-jG52XwuQ');
-        this.page.addChild(video);
-        const note = new NoteComponent('Note Title', 'Note Body');
-        this.page.addChild(note);
-        const todo = new TodoComponent('Todo Title', 'Todo Item');
-        this.page.addChild(todo);
-        const imageBtn = document.querySelector('#new-image');
-        imageBtn.addEventListener('click', () => {
+        this.bindElementToDialog('#new-image', MediaSectionInput, (input) => new ImageComponent(input.title, input.url));
+        this.bindElementToDialog('#new-video', MediaSectionInput, (input) => new VideoComponent(input.title, input.url));
+        this.bindElementToDialog('#new-note', TextSectionInput, (input) => new NoteComponent(input.title, input.body));
+        this.bindElementToDialog('#new-todo', TextSectionInput, (input) => new TodoComponent(input.title, input.body));
+    }
+    bindElementToDialog(selector, InputComponent, makeSection) {
+        const element = document.querySelector(selector);
+        element.addEventListener('click', () => {
             const dialog = new InputDialog();
+            const input = new InputComponent();
+            dialog.addChild(input);
+            dialog.attachTo(this.dialogRoot);
             dialog.setOnCloseListener(() => {
-                dialog.removeFrom(document.body);
+                dialog.removeFrom(this.dialogRoot);
             });
             dialog.setOnSubmitListener(() => {
-                dialog.removeFrom(document.body);
+                const image = makeSection(input);
+                this.page.addChild(image);
+                dialog.removeFrom(this.dialogRoot);
             });
-            dialog.attachTo(document.body);
         });
     }
 }
-new App(document.querySelector('.document'));
+new App(document.querySelector('.document'), document.body);
--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,73 @@
+import {TextSectionInput} from './components/dialog/input/text-input.js';
 import {VideoComponent} from './components/page/item/video.js';
 import {TodoComponent} from './components/page/item/todo.js';
 import {NoteComponent} from './components/page/item/note.js';
 import {ImageComponent} from './components/page/item/image.js';
 import {Composable, PageComponent, PageItemComponent} from './components/page/page.js';
 import {Component} from './components/component.js';
-import {InputDialog} from './components/dialog/dialog.js';
+import {InputDialog, MediaData, TextData} from './components/dialog/dialog.js';
+import {MediaSectionInput} from './components/dialog/input/media-input.js';
 
+type InputComponentConstructor<T = (MediaData | TextData) & Component> = {
+  new (): T;
+};
 class App {
   private readonly page: Component & Composable; //page는 Component중에 하나이고, Composable이라는 interface를 구현한 것이다.
-  constructor(appRoot: HTMLElement) {
+  constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
     this.page = new PageComponent(PageItemComponent); //인스턴스를 만들고
     this.page.attachTo(appRoot); //이 인스턴스 안의 attatchTo메소드로 element추가
 
-    const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-    this.page.addChild(image);
+    this.bindElementToDialog<MediaSectionInput>(
+      '#new-image',
+      MediaSectionInput,
+      (input: MediaSectionInput) => new ImageComponent(input.title, input.url)
+    );
 
-    const video = new VideoComponent('Video Title', 'https://youtu.be/K3-jG52XwuQ');
-    this.page.addChild(video);
+    this.bindElementToDialog<MediaSectionInput>(
+      '#new-video',
+      MediaSectionInput,
+      (input: MediaSectionInput) => new VideoComponent(input.title, input.url)
+    );
 
-    const note = new NoteComponent('Note Title', 'Note Body');
-    this.page.addChild(note);
+    this.bindElementToDialog<TextSectionInput>(
+      '#new-note',
+      TextSectionInput,
+      (input: TextSectionInput) => new NoteComponent(input.title, input.body)
+    );
 
-    const todo = new TodoComponent('Todo Title', 'Todo Item');
-    this.page.addChild(todo);
+    this.bindElementToDialog<TextSectionInput>(
+      '#new-todo',
+      TextSectionInput,
+      (input: TextSectionInput) => new TodoComponent(input.title, input.body)
+    );
+  }
 
-    const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
-    imageBtn.addEventListener('click', () => {
+  private bindElementToDialog<T extends (MediaData | TextData) & Component>(
+    //MediaData 또는 TextData를 가질 수 있고 Component를 구현한 아이를 인자로 받는다.
+    selector: string,
+    InputComponent: InputComponentConstructor<T>,
+    makeSection: (input: T) => Component
+  ) {
+    const element = document.querySelector(selector)! as HTMLButtonElement;
+    element.addEventListener('click', () => {
       const dialog = new InputDialog();
+      const input = new InputComponent();
+      dialog.addChild(input);
+      dialog.attachTo(this.dialogRoot);
+
       dialog.setOnCloseListener(() => {
-        dialog.removeFrom(document.body);
+        dialog.removeFrom(this.dialogRoot);
       });
       dialog.setOnSubmitListener(() => {
-        //섹션을 만들어서 페이지에 추가 해준다.
-        dialog.removeFrom(document.body);
+        const image = makeSection(input);
+        this.page.addChild(image);
+        dialog.removeFrom(this.dialogRoot);
       });
-
-      dialog.attachTo(document.body);
     });
   }
 }
 
-new App(document.querySelector('.document')! as HTMLElement);
+new App(document.querySelector('.document')! as HTMLElement, document.body);
 /* querySelector는 HTMLElement나 null을 리턴할 수 있는 api이다.
 우리는 querySelector안에 동적인 요소를 넣을 것이 아니라 확실하게 정해져있는
 index.html 안에 있는 <div class="document">에 넣을 것이기 때문에
